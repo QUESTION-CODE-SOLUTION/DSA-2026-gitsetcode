a99cc1c605fd2f7f9bf248f7bfe6efc5c853359c
Smriti Jain <132169208+Smritii-jain@users.noreply.github.com>
2023-11-06 00:09:55 +0530

    Update numberpattern5.cpp

--- Smriti_Jain/Code/03-11-23/numberpattern5.cpp
+++ Smriti_Jain/Code/03-11-23/numberpattern5.cpp
@@ -17,3 +17,16 @@ int main() {
     
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+int main() {
+    for(int i=1;i<=5;i++){
+        for(int j=6-i;j>=1;j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
