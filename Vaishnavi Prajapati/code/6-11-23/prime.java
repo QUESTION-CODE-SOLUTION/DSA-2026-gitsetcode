37d73dd7a9b9f4827672de4400a46dfea62f3aa4
Vaishnavee Prajapati <131327874+vaishnavee1424@users.noreply.github.com>
2023-11-06 23:17:46 +0530

    Create prime.java

---
+++ Vaishnavi Prajapati/code/6-11-23/prime.java
@@ -0,0 +1,21 @@
+public class prime {
+    public static void main(String[]args){
+        // how isne 2 ko ek prime number kyu liya
+       int a=2; 
+       int count=0;
+       
+       for(int i=2;i<a;i++){
+        if(a%i==0){
+            count++;
+        }
+       }
+       if(count>0|| a==1){
+         System.out.println("not a prime");
+       }
+       else{
+        System.out.println("prime");
+       }
+    
+    }}
+     
+    
