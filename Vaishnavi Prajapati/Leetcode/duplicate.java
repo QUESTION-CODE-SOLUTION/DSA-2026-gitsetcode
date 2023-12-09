ad3c75d3022660aee3301a3434c3cddbfce4f1cd
Vaishnavee Prajapati <131327874+vaishnavee1424@users.noreply.github.com>
2023-12-09 14:48:51 +0530

    Add files via upload

--- Vaishnavi Prajapati/Leetcode/duplicate.java
+++ Vaishnavi Prajapati/Leetcode/duplicate.java
@@ -1,13 +1,12 @@
-// class duplicate {
-//     public static void main (String []args){
-// int nums[]={1,2,3,1};
-//        Arrays.sort(nums);
-//         for(int i=1;i<nums.length;i++){
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
             
-//                 if(nums[i-1]==nums[i]){
-//                     return true;
-//                 }
-//             }
-//             return false;
-//         }
-//     }
+                if(nums[i-1]==nums[i]){
+                    return true;
+                }
+            }
+            return false;
+        }
+}
